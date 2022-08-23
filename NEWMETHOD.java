15d45d1d31cc6e5d552be95bf2c7a4d6226c0586
SriramR30072002 <109788435+SriramR30072002@users.noreply.github.com>
2022-08-23 14:59:34 +0530

    Add files via upload

---
+++ NEWMETHOD.java
@@ -0,0 +1,17 @@
+package Sripackage;
+
+public class NEWMETHOD {
+	
+	public void mymethod() {
+		
+		System.out.println("My New Method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Method");
+		NEWMETHOD o=new NEWMETHOD();
+		 o.mymethod();
+	}
+
+}
